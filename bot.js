98e5ac9d8d4ae6fc2024258454ae5c0d205e5431
Brent Williams <functnal@gmail.com>
2022-05-07 00:51:45 -0400

    Command setup

--- bot.js
+++ bot.js
@@ -1,34 +1,72 @@
 //Requried Dependencies
-const { Client, Intents } = require(`discord.js`);
-const { config } = require(`./config.json`);
+const { Client, Collection, Intents } = require('discord.js');
+const config = require('./config.json');
+const fs = require('node:fs');
 
 //List of DISCORD INTENTS
-const { INTENTS } = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGES_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_SCHEDULED_EVENTS];
+const INTENTS  = 
+[
+	Intents.FLAGS.GUILDS,
+	Intents.FLAGS.GUILD_MEMBERS,
+	Intents.FLAGS.GUILD_BANS,
+	Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+	Intents.FLAGS.GUILD_INTEGRATIONS,
+	Intents.FLAGS.GUILD_WEBHOOKS,
+	Intents.FLAGS.GUILD_INVITES,
+	Intents.FLAGS.GUILD_VOICE_STATES,
+	Intents.FLAGS.GUILD_PRESENCES,
+	Intents.FLAGS.GUILD_MESSAGES,
+	Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+	Intents.FLAGS.GUILD_MESSAGE_TYPING,
+	Intents.FLAGS.DIRECT_MESSAGES,
+	Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+	Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+	Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
+];
 
 //New Discord Client
 const client = new Client({ intents: INTENTS });
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles)
+{
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 //Once time only event listener.  
-client.once(`ready`, () =>
+client.once('ready', () =>
 {
-	console.log(`Client Initialized...`);
-	console.log(client);
+	console.log('Client Initialized...');
 
-	client.channels.fetch(config.baseChannel)
-		.then(channel =>
-		{
-			channel.send(`what's up fuckers? <:Pepepunch:783434609539022881>`);
-		});
+	// client.channels.fetch(config.baseChannel)
+	// 	.then(channel =>
+	// 	{
+	// 		channel.send('what\'s up fuckers? <:Pepepunch:783434609539022881>');
+	// 	});
 });
 
-client.on(`interactionCreate`, async interaction => 
+client.on('interactionCreate', async interaction => 
 {
 	if(!interaction.isCommand()) return;
 
-	const { commandName  } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if(commandName === `ping`)
-		await interaction.reply(`Pong!`);
-	else if(commandName === `beep`)
-		await interaction.reply(`Boop!`);
+	if(!command) return;
+
+	try 
+	{
+		await command.execute(interaction);
+	} 
+	catch ({ error }) 
+	{
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command.', ephermeral: true });
+	}
 });
+
+client.login(config.token);
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,11 +1,23 @@
-const { REST } = require(`@discordjs/rest`);
-const { Routes } = require(`discord-api-types/v10`);
-const { token, clientId, guildId } = require(`./config.json`);
+const fs = require('node:fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token, clientId, guildId } = require('./config.json');
 
+//Creating commands
 const commands = [];
+//Getting all files in the commands dir with .js ending
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-const rest = new REST({ vesrion: `10` }).setToken(token);
+//Looping through each file getting the module.export data
+//and then pushing it to commands
+for(const file of commandFiles)
+{
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ vesrion: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log(`Successfully registered application commands.`))
+	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
